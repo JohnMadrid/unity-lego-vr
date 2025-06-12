7ed3793e4ff01241c476ee8a737f3b953a3bd3fc
JohannaBeck <johbeck@uos.de>
2025-06-12 14:35:30 +0200

    Fixing snapping scripts

--- Assets/Scripts/SnapPoint.cs
+++ Assets/Scripts/SnapPoint.cs
@@ -5,15 +5,15 @@ public class SnapPoint : MonoBehaviour
     public enum Orientation { Stud, Recept }
     public Orientation orientation;
 
-    public Transform snapTransform; // The point to align when snapping
-    public GameObject parentBrick; // The brick object this SnapPoint belongs to
+    public Transform snapTransform; // point for aligning
+    public GameObject parentBrick;  // auto-assigned in Awake
 
     void Awake()
     {
         if (parentBrick == null)
             parentBrick = transform.parent.gameObject;
         if (snapTransform == null)
-            snapTransform = this.transform; // default to the transform itself
+            snapTransform = this.transform; // default
     }
 
     public float GetDistanceTo(Vector3 position)
